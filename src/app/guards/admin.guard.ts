15f4e40ed7d9f13221cbadc20e5ba7ae89c3dd71
sebastianaguirre17 <sebaaguirre2012@gmail.com>
2020-10-22 18:52:07 -0300

    Mensaje con rutas incorrectas

--- src/app/guards/admin.guard.ts
+++ src/app/guards/admin.guard.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { CanActivate } from '@angular/router';
 import { Observable } from 'rxjs';
 import { map, tap, take } from 'rxjs/operators';
+import Swal from 'sweetalert2';
 import { AuthService } from '../services/auth.service';
 
 @Injectable({
@@ -18,7 +19,13 @@ export class AdminGuard implements CanActivate {
             map((user) => user && this.auth.isAdmin(user)),
             tap((canAdmin) => {
                 if(!canAdmin) {
-                    console.log('Acceso denegado!');
+                    Swal.fire({
+                        text: 'Debe ser administrador para acceder a la ruta especificada.',
+                        allowOutsideClick: false,
+                        showCloseButton: true,
+                        icon: 'error',
+                        title: 'Acceso denegado'
+                    });
                 }
             })
         )
