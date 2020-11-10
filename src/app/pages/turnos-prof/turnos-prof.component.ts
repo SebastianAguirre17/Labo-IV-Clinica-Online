49bb9f38ca820860e6fd3e7ad73a9e275945fe55
SebastianAguirre17 <sebaaguirre2012@gmail.com>
2020-11-10 20:11:04 -0300

    Comenzando carga dinámica

--- src/app/pages/turnos-prof/turnos-prof.component.ts
+++ src/app/pages/turnos-prof/turnos-prof.component.ts
@@ -18,6 +18,19 @@ import Swal from 'sweetalert2';
 export class TurnosProfComponent implements OnInit {
 
 
+    public historiaForm = this.fb.group({
+        age: ['', [Validators.required, Validators.minLength(1)]],
+        peso: ['', [Validators.required, Validators.minLength(2), Validators.min(5), Validators.max(150)]],
+        temp: ['', [Validators.required, Validators.minLength(2), Validators.min(30), Validators.max(42)]],
+        presion: ['', [Validators.required, Validators.minLength(2)]],
+        clave1: [''],
+        clave2: [''],
+        clave3: [''],
+        valor1: [''],
+        valor2: [''],
+        valor3: ['']
+    });
+
     listado: any[] = [];
     listadoPendientes: any[] = [];
     isLoading = false;
@@ -30,8 +43,24 @@ export class TurnosProfComponent implements OnInit {
     });
 
     constructor(private dbService: DataService,
-                private auth: AuthService,
-                private fb: FormBuilder) {
+        private auth: AuthService,
+        private fb: FormBuilder) {
+    }
+
+    get invalidAge() {
+        return this.historiaForm.get('age').invalid && this.historiaForm.get('age').touched;
+    }
+
+    get invalidPeso() {
+        return this.historiaForm.get('peso').invalid && this.historiaForm.get('peso').touched;
+    }
+
+    get invalidPresion() {
+        return this.historiaForm.get('presion').invalid && this.historiaForm.get('presion').touched;
+    }
+
+    get invalidTemp() {
+        return this.historiaForm.get('temp').invalid && this.historiaForm.get('temp').touched;
     }
 
     ngOnInit() {
@@ -83,19 +112,18 @@ export class TurnosProfComponent implements OnInit {
     }
 
     atenderPaciente(turno: Turno) {
-        turno.estado = 'FINALIZADO';
-        this.dbService.updateOne(turno, 'turnos');
+        this.turnoSeleccionado = turno;
     }
 
     seleccionarTurno(turno: Turno) {
         this.turnoSeleccionado = turno;
-        if(this.turnoSeleccionado.resenia) {
+        if (this.turnoSeleccionado.resenia) {
             this.reseniaForm.setValue({
                 text: this.turnoSeleccionado.resenia
             });
         };
     }
-    
+
     get invalidText() {
         return this.reseniaForm.get('text').invalid && this.reseniaForm.get('text').touched;
     }
@@ -137,12 +165,26 @@ export class TurnosProfComponent implements OnInit {
 
     generarExel(id: string) {
         let element = document.getElementById(id);
-        const ws: XLSX.WorkSheet =XLSX.utils.table_to_sheet(element);
-     
+        const ws: XLSX.WorkSheet = XLSX.utils.table_to_sheet(element);
+
         const wb: XLSX.WorkBook = XLSX.utils.book_new();
         XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
-     
+
         XLSX.writeFile(wb, 'Turnos.xlsx');
+    }
+
+    guardarHistoria() {
+
+        if (this.historiaForm.invalid) {
+            return Object.values(this.historiaForm.controls).forEach(control => {
+                if (control instanceof FormGroup)
+                    Object.values(control.controls).forEach(control => control.markAsTouched());
+                else
+                    control.markAsTouched();
+            });
+        }
+
+        console.log(this.historiaForm.value);
     }
 }
 
